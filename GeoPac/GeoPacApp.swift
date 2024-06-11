c8e0cf3b900944d43ed9156eed498681dcbfdd8f
Ria <riabodar1@gmail.com>
2024-06-11 09:50:55 -0500

    Initial Commit

---
+++ GeoPac/GeoPacApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoPacApp.swift
+//  GeoPac
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GeoPacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
